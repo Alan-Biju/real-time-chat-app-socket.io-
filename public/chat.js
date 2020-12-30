d4f5ec2e2d619f8b98ecf512aaf1af8a2a151c25
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2020-12-30 16:48:22 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -3,19 +3,41 @@ const chat=document.querySelector('.chat-messages');
 const type=document.querySelector('#typing');
 const msg=document.querySelector('#msg');
 const btn=document.querySelector('.btn');
+/*-----------------------------*/
+
+const user=prompt("Whats your name::")||"Anonymous";
+
 
 const socket = io();
 socket.on('message', (data)=>{
+    console.log(data);
+    const div = document.createElement('div');
+  div.classList.add('message');
+ div.innerHTML=`<p class="text">
+ ${data}
+</p>`
+ 
+  
+  document.querySelector('.chat-messages').appendChild(div)
+  
+chat.scrollTop = chat.scrollHeight;
+
+})
+
+
+socket.on('messages', (data)=>{
     console.log(data);
     chats(data);
 chat.scrollTop = chat.scrollHeight;
 
 })
 
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-   
-  socket.emit('chatmessage',msg.value);
+   const data={message:msg.value,name:user}
+   console.log(data);
+  socket.emit('chatmessage',data);
   msg.value='';
 
 
@@ -23,7 +45,7 @@ form.addEventListener('submit',(e)=>{
 } )
 
 
-msg.addEventListener('input',()=>{
+msg.addEventListener('keypress',()=>{
   socket.emit('keydown',"typing.......");
 })
 
@@ -42,10 +64,11 @@ msg.addEventListener('keypress',(e)=>{
   })
 
 function chats(message){
+ 
  const div = document.createElement('div');
   div.classList.add('message');
- div.innerHTML=`<p class="text">
- ${message}
+ div.innerHTML=`<p class='meta'>${message.name}</p><p class="text">
+ ${message.message}
 </p>`
  
   
